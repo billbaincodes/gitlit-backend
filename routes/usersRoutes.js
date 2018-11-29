c67e2c8b2afc54ef0f35e0b5a3595ede60272e71
hillrg22 <hillrg22@gmail.com>
2018-11-29 12:55:07 -0700

    added npm bcryptjs and dotenv packages, created auth routes file

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -60,6 +60,17 @@ router.delete('/:id', (req,res,next) => {
 })
 
 
+//signup
+router.post('signup', function(req,res,next){
+  var hash = bcrypt.hashSync(req.Params.Password, 8)
+  Users().insert({
+    email: req.params.email,
+    password: hash
+  }, 'id').then(function(result){
+    res.redirect('/signin')
+  })
+})
+
 
 
 
