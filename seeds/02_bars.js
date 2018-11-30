6d1e15f9185239c07a4c91e84dac9d2e2706d7ba
bill <williamandrewbain@gmail.com>
2018-11-29 21:46:14 -0700

    Updated bar seed

--- seeds/02_bars.js
+++ seeds/02_bars.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex.raw('DELETE FROM "bars"; ALTER SEQUENCE bars_id_seq RESTART WITH 4;')
+  return knex.raw('DELETE FROM "bars"; ALTER SEQUENCE bars_id_seq RESTART WITH 5;')
     .then(function () {
       // Inserts seed entries
       return knex('bars').insert([
@@ -21,6 +21,12 @@ exports.seed = function(knex, Promise) {
           id: 3,
           name: 'Highland Tap and Burger',
           location: '2219 W 32nd Ave.',
+          user_count: 0
+        },
+        {
+          id: 4,
+          name: 'Forest Room 5',
+          location: '2532 15th St',
           user_count: 0
         }
       ])
